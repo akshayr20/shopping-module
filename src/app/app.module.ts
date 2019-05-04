07e0be7be92fbe066e623026d8b290bbbdec514f
akshayr20 <akshay.rajput5394@gmail.com>
2019-05-04 11:14:29 +0530

    added essential components for the applications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { EditCartItemComponent } from './shopping-cart/edit-cart-item/edit-cart-item.component';
+import { CartItemComponent } from './shopping-cart/cart-item/cart-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent,
+    EditCartItemComponent,
+    CartItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shopping-cart/edit-cart-item/edit-cart-item.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditCartItemComponent } from './edit-cart-item.component';
+
+describe('EditCartItemComponent', () => {
+  let component: EditCartItemComponent;
+  let fixture: ComponentFixture<EditCartItemComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EditCartItemComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditCartItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

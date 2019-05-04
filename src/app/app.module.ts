00f0892c3211a958afba4223b8df54bd36043e86
akshayr20 <akshay.rajput5394@gmail.com>
2019-05-04 12:17:50 +0530

    completed work on UI  for shopping details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { EditCartItemComponent } from './shopping-cart/edit-cart-item/edit-cart-item.component';
 import { CartItemComponent } from './shopping-cart/cart-item/cart-item.component';
+import { CartDetailComponent } from './shopping-cart/cart-detail/cart-detail.component';
+import { ContactUsComponent } from './shopping-cart/cart-detail/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingCartComponent,
     EditCartItemComponent,
-    CartItemComponent
+    CartItemComponent,
+    CartDetailComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
